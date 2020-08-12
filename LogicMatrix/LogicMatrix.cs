fea8dba0210d1c750674f9e5235cbb6c60a42ae6
Hiroaki Kawata <maemaewater@gmail.com>
2020-08-12 11:28:05 +0900

    first commit

---
+++ LogicMatrix/LogicMatrix.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace LogicMatrix
+{
+    class LogicMatrix
+    {
+        private float[,] m;
+        public Func<float, float>[,] logics;
+
+        public float this[int i, int j]
+        {
+            set
+            {
+                this.m[i, j] = value;
+            }
+
+            get
+            {
+                return logics[i, j](m[i, j]);
+            }
+        }
+
+        public int RowLength
+        {
+            get { return this.m.GetLength(0); }
+        }
+
+        public int ColLength
+        {
+            get { return this.m.GetLength(1); }
+        }
+
+        public LogicMatrix(int row, int col)
+        {
+            this.m = new float[row, col];
+
+            this.logics = new Func<float, float>[row, col];
+
+            for (int j = 0; j < col; j++)
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    this.logics[i, j] = v => 0;
+                }
+            }
+        }        
+    }
+}
---
+++ LogicMatrix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LogicMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogicMatrix m = new LogicMatrix(2, 3);
+
+            m.logics[1, 1] = x =>
+            {
+                if (x > 5)
+                {
+                    return 1.0F;
+                }
+                else
+                {
+                    return 0.0F;
+                }
+            };
+
+            for (int y = 0; y < m.ColLength; y++)
+            {
+                for (int x = 0; x < m.RowLength; x++)
+                {
+                    m[x, y] = 10;
+                }
+            }
+
+            for (int y = 0; y < m.ColLength; y++)
+            {
+                for (int x = 0; x < m.RowLength; x++)
+                {
+                    Console.WriteLine("[{0}, {1}] = {2}", x, y, m[x, y]);
+                }
+            }
+
+            Console.WriteLine("(row, col) = ({0}, {1})", m.RowLength, m.ColLength);
+        }
+    }
+}
